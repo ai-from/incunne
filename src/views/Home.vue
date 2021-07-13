228b8f192b654f6354dd5708e21fd89016363bec
Aifrom <codemusic2020@gmail.com>
2021-07-13 13:39:12 +0300

    clean Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    Home
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home',
-  components: {
-  }
+  name: 'Home'
 }
 </script>
