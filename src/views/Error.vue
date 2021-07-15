c0414725d485d504720a8afa00b09b40a9fc46fe
Aifrom <codemusic2020@gmail.com>
2021-07-15 04:15:13 +0300

    add Error.vue

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    Error
+  <div class="error-page indent">
+    <PageNotFound />
   </div>
 </template>
 
 <script>
+import PageNotFound from "../components/error/PageNotFound"
 export default {
-  name: 'Error'
+  name: 'Error',
+  components: {
+    PageNotFound
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+.error-page
+  .logo
+    margin: 0 auto 80px
+  #error-pic
+    width: 70%
+    height: auto
+    max-height: 300px
+    display: grid
+    margin: 50px auto
+</style>
