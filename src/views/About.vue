fae1ce122981486991a799917e77ab8ba92d308c
Aifrom <codemusic2020@gmail.com>
2021-07-15 04:01:38 +0300

    add About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,97 @@
 <template>
-  <div>
-    About
+  <div class="about-page indent">
+
+    <div class="about">
+
+      <div class="about__links">
+        <a
+          class="about__link"
+          v-for="(link, index) in links"
+          :key="index"
+          :href="link.href"
+          :alt="link.alt"
+          :title="link.title"
+          target="_blank"
+          @mouseover="link.isHover = true"
+          @mouseleave="link.isHover = false"
+          :style="{
+            color: link.isHover ? `rgb(${theme})` : '',
+            outline: link.isHover ? `1px solid rgba(${theme}, .3)` : ''
+          }"
+        >
+          <span class="about__link-name">{{ link.name }}</span>
+          <img
+            class="about__link-img"
+            :src="require(`./../assets/img/about/${link.innerName}.svg`)"
+            :alt="`${link.name}`"
+            :title="`${link.name}`"
+          >
+        </a>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
-  name: 'About'
+  name: 'About',
+  data() {
+    return {
+      links: [
+        {name: 'VK page', innerName: 'vk-page', href: 'https://vk.com/code.music', alt: 'Incunne | VK page', title: 'Incunne | VK page', isHover: false},
+        {name: 'VK community', innerName: 'vk-community', href: 'https://vk.com/incunne', alt: 'Incunne | VK community', title: 'Incunne | VK community', isHover: false},
+        {name: 'SoundCloud', innerName: 'sound-cloud', href: 'https://soundcloud.com/incunne', alt: 'Incunne | SoundCloud', title: 'Incunne | SoundCloud', isHover: false},
+        {name: 'RealMusic', innerName: 'real-music', href: 'https://www.realrocks.ru/incunne/', alt: 'Incunne | RealMusic', title: 'Incunne | RealMusic', isHover: false},
+        {name: 'PromoDj', innerName: 'promo-dj', href: 'https://promodj.com/incunne', alt: 'Incunne | PromoDj', title: 'Incunne | PromoDj', isHover: false},
+        {name: 'YouTube', innerName: 'youtube', href: 'https://www.youtube.com/user/Incunne', alt: 'Incunne | YouTube', title: 'Incunne | YouTube', isHover: false},
+        {name: 'Telegram', innerName: 'telegram', href: 'https://t.me/incunne', alt: 'Incunne | Telegram', title: 'Incunne | Telegram', isHover: false},
+        {name: 'Instagram', innerName: 'instagram', href: 'https://instagram.com/hughes.stanley?r=nametag', alt: 'Incunne | Instagram', title: 'Incunne | Instagram', isHover: false}
+      ]
+    }
+  },
+  computed: {
+    ...mapState('common', {
+      theme: state => state.theme
+    })
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+.about-page
+  .about
+    display: grid
+    grid-template-rows: 1fr min-content
+    &__links
+      display: grid
+      grid-template-columns: repeat(auto-fill, minmax(200px, min-content))
+      grid-template-rows: repeat(5, min-content)
+      grid-gap: 50px
+      align-content: space-between
+    &__link
+      width: 200px
+      padding: 8px 10px
+      background: rgba($black, .5)
+      display: grid
+      grid-template-columns: 1fr min-content
+      grid-gap: 10px
+      align-items: center
+      outline: 1px solid rgba($grey, .1)
+      &-name
+      &-img
+        display: block
+        width: 34px
+
+@media screen and (max-width: 576px)
+  .about-page
+    .about
+      &__links
+        grid-gap: 20px
+        grid-template-columns: 1fr
+      &__link
+        width: 100%
+        max-width: 400px
+</style>
